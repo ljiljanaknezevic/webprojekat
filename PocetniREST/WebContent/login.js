120765b206760fb0538f4bd06f6d1466bcfb7a72
milica-okiljevic <58478503+milica-okiljevic@users.noreply.github.com>
2020-09-04 11:40:17 +0200

    uodate

--- PocetniREST/WebContent/login.js
+++ PocetniREST/WebContent/login.js
@@ -1,18 +1,5 @@
 var userLogged = 'none';
-function drawApartments(data){
-	let temp='';
-	for (i in data){
-		temp+=`<tr id="`+data[i].id+`">
-			<td>`+data[i].status+`</td>
-			<td>`+data[i].type+`</td>
-			<td>`+data[i].location+`</td>
-			<td>`+data[i].numberOfRooms+`</td>
-			<td>`+data[i].numberOfGuest+`</td>
-			<td>`+data[i].price+`</td>
-			<td><button id="view-comment" class="btn btn-primary">View comments</button></td></tr>`;
-	}
-	$('#apartmentsTable').html(temp);
-}
+
 
 $(document).ready(function(){
 	function currentUser(){
@@ -65,20 +52,20 @@ $(document).ready(function(){
 			}
 		})
 	})
-	$('a[href="#apartments"]').click(function(){
-		$('form#login').attr("hidden", true);
-		$('#content').attr("hidden", false);
-		
-		$.ajax({
-			url:'ProjectRents/allActiveApartments',
-			type : "GET",
-			contentType : 'application/json',
-			success : function(data){
-				drawApartments(data)
-			}
-			
-		})
-	})
+//	$('a[href="#apartments"]').click(function(){
+//		$('form#login').attr("hidden", true);
+//		$('#content').attr("hidden", false);
+//		
+//		$.ajax({
+//			url:'ProjectRents/allActiveApartments',
+//			type : "GET",
+//			contentType : 'application/json',
+//			success : function(data){
+//				drawApartments(data)
+//			}
+//			
+//		})
+//	})
 })
 
 
